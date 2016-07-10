cb38e075c5b9e19da8f762ade5528dd079b2d91d
Oxyno-zeta <havrileckalexandre@gmail.com>
2016-07-10 20:21:56 +0200

    [Backend][Model] Add Project and CrashLog model

---
+++ src/models/crashLogModel.js
@@ -0,0 +1,42 @@
+/*
+ * Author: Alexandre Havrileck (Oxyno-zeta) 
+ * Date: 08/07/16
+ * Licence: See Readme
+ */
+
+/* ************************************* */
+/* ********       REQUIRE       ******** */
+/* ************************************* */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const crashLogSchema = new Schema({
+	ver: String,
+	platform: String,
+	process_type: String,
+	guid: String,
+	_version: String,
+	_productName: String,
+	prod: String,
+	_companyName: String,
+	upload_file_minidump: String,
+	extra: {},
+	project: Schema.Types.ObjectId
+});
+const CrashLog = mongoose.model('CrashLog', crashLogSchema);
+
+/* ************************************* */
+/* ********        EXPORTS      ******** */
+/* ************************************* */
+
+module.exports = {
+	CrashLog: CrashLog
+};
+
+/* ************************************* */
+/* ********  PRIVATE FUNCTIONS  ******** */
+/* ************************************* */
+
+
+/* ************************************* */
+/* ********   PUBLIC FUNCTIONS  ******** */
+/* ************************************* */
---
+++ src/models/projectModel.js
@@ -0,0 +1,35 @@
+/*
+ * Author: Alexandre Havrileck (Oxyno-zeta) 
+ * Date: 08/07/16
+ * Licence: See Readme
+ */
+
+/* ************************************* */
+/* ********       REQUIRE       ******** */
+/* ************************************* */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const projectSchema = new Schema({
+	name: String,
+	projectUrl: String,
+	crashLogList: [Schema.Types.ObjectId] // Crash log Model lists
+});
+const Project = mongoose.model('Project', projectSchema);
+
+/* ************************************* */
+/* ********        EXPORTS      ******** */
+/* ************************************* */
+
+module.exports = {
+	Project: Project
+};
+
+/* ************************************* */
+/* ********  PRIVATE FUNCTIONS  ******** */
+/* ************************************* */
+
+
+/* ************************************* */
+/* ********   PUBLIC FUNCTIONS  ******** */
+/* ************************************* */
+
